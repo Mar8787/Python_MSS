40cae89da786fbd4078ac61f8c13abdcea1dd7d2
Mar8787 <mss7@outlook.com>
2025-11-05 13:59:36 +0100

    Added exercise 22

--- UD03_EJ_UD01/EJ_21.py
+++ UD03_EJ_UD01/EJ_21.py
@@ -1,17 +1,17 @@
 #* Ejercicio 21: Crea un programa que lea 100 números no nulos y luego muestre un mensaje de si ha leído número negativo o no
 
 neg = 0
-pos = 0
 contador = 0
-while contador < 5:
+while contador < 100:
     num = int(input("Introduce un número no nulo: "))
     if num == 0:
         print("El cero no es un número válido.")
     elif num > 0:
         contador += 1
-        pos += 1
     else:
         contador += 1
         neg += 1
-print(f"Números positivos: {pos}")
-print(f"Números negativos: {neg}")
+if neg > 0:
+    print("Se ha leído al menos un número negativo.")
+else:
+    print("No se ha leído ningún número negativo.")
--- UD03_EJ_UD01/EJ_22.py
+++ UD03_EJ_UD01/EJ_22.py
@@ -2,4 +2,16 @@
 
 pos = 0
 neg = 0
-num = int(input("Introduce un número no nulo: "))
+contador = 0
+while contador < 5:
+    num = int(input("Introduce un número no nulo: "))
+    if num == 0:
+        print("El cero no es un número válido.")
+    elif num > 0:
+        contador += 1
+        pos += 1
+    else:
+        contador += 1
+        neg += 1
+print(f"Números positivos: {pos}")
+print(f"Números negativos: {neg}")
