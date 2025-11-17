fe984360ed8844663e44e6613ac326cdd45dfd2b
Mar8787 <mss7@outlook.com>
2025-11-17 10:01:52 +0100

    Creadas carpetas UD03 y 14 15

--- UD03_EJ_UD01/EJ_27.py
+++ UD03_EJ_UD01/EJ_27.py
@@ -4,3 +4,12 @@ nota = 0
 contador = 0
 while nota != -1:
     nota = float(input("Introduce una nota (o -1 para terminar): "))
+    if 0 <= nota <= 10:
+        if nota == 10:
+            contador += 1
+    else:
+        print(f"Introduce una nota válida)")
+if contador > 0:
+    print("Detectada al menos una nota con valor 10.")
+else:
+    print("No se ha detectado ninguna nota con valor 10.")
