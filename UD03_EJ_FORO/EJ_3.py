93164f85ac6f5677878216e408e2531f4e3d09b6
“Mar8787” <mss7@outlook.com>
2025-11-19 21:46:44 +0100

    ADD ej_3 foro (cuadrados)

---
+++ UD03_EJ_FORO/EJ_3.py
@@ -0,0 +1,9 @@
+altura = int(input("Introduce la altura de la figura: "))
+
+#Cabecera de la figura
+print(((altura * 2) + 1) * ("* "))
+
+#Altura de la figura
+for i in range(1, altura + 1):
+    print("*   " * (altura + 1))
+    print("* " * (((altura + 1 ) * 2) - 1))
