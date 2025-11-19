7f3e54c201295f218c852f7d8fecd1132d78fa08
“Mar8787” <mss7@outlook.com>
2025-11-19 18:57:04 +0100

    EJ_1 foro huecos conseguidos

--- UD03_EJ_FORO/EJ_1.py
+++ UD03_EJ_FORO/EJ_1.py
@@ -1,21 +1,28 @@
 altura = int(input("Introduce la altura de la figura: "))
-#Cabecera de la figura
-print(altura*(" ") + "*")
 
+#Cabecera de la figura
+print((altura+1)*(" ") + "*")
+
+#Esto nos da la altura
 for i in range(1, altura + 1):
-    #construimos los espacios
+
+    #construimos los espacios delanteros
     for j in range(i, altura):
         print(" ", end="")
-    #construimos el *
-    for k in range(1, i +2):
-        print("*", end="")
-    print("")
-#Segunda parte de la figura
 
-
-for i in range(1, altura + 1):
-    for j in range(0-1, i-1):
+    #construimos el * los espacios de en mendio y el * final
+    print("*", end="")
+    for k in range(1, i + 1):
         print(" ", end="")
-    for k in range(0, (altura +2) - (i+1) ):
-        print("*", end="")
+    print("*", end="")
+    print("")
+
+#Segunda parte de la figura
+for i in range(1, altura + 1):
+    for j in range(0 - 1, i - 1):
+        print(" ", end="")
+    print("*", end="")
+    for k in range(0, (altura + 1) - (i + 1)):
+        print(" ", end="")
+    print("*", end="")
     print("")
--- UD03_EJ_FORO/EJ_2.py
+++ UD03_EJ_FORO/EJ_2.py
@@ -5,6 +5,6 @@ for i in range(1, altura-1):
     print("4",end="")
     print(" "*(i-1),end="")
     print("4")
-    
+
 # Base figura
 print("4"*altura)
