98c10f174fe3f1ba6a5dea8aab51b0c345212234
Mar8787 <mss7@outlook.com>
2025-11-19 14:10:48 +0100

    ADD ejercicio 1 foro

---
+++ UD03_EJ_FORO/EJ_1.py
@@ -0,0 +1,9 @@
+altura = int(input("Introduce la altura de la figura: "))
+for i in range(1, altura + 1):
+    #construimos los primeros espacios
+    for j in range(i, altura):
+        print(" ", end="")
+    #construimos el *
+    for k in range(1, i +2):
+        print("*", end="")
+    print("")
