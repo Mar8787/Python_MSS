282a5a93d9437e883ca05c7cd301666e3357587a
“Mar8787” <mss7@outlook.com>
2025-11-19 17:57:09 +0100

    Forma completa sin espacios

--- UD03_EJ_FORO/EJ_1.py
+++ UD03_EJ_FORO/EJ_1.py
@@ -3,11 +3,19 @@ altura = int(input("Introduce la altura de la figura: "))
 print(altura*(" ") + "*")
 
 for i in range(1, altura + 1):
-    #construimos los primeros espacios
+    #construimos los espacios
     for j in range(i, altura):
         print(" ", end="")
     #construimos el *
     for k in range(1, i +2):
         print("*", end="")
     print("")
-    
+#Segunda parte de la figura
+
+
+for i in range(1, altura + 1):
+    for j in range(0-1, i-1):
+        print(" ", end="")
+    for k in range(0, (altura +2) - (i+1) ):
+        print("*", end="")
+    print("")
