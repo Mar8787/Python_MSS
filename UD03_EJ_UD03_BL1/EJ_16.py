53b346e0bead06362309ccd3dbf324c88e812aa2
Mar8787 <mss7@outlook.com>
2025-11-28 09:27:07 +0100

    Comenzando el 16 bl1

--- UD03_EJ_UD03_BL1/EJ_16.py
+++ UD03_EJ_UD03_BL1/EJ_16.py
@@ -1,1 +1,8 @@
 # 16. Escriba un programa que pida un número entre 0 y 99999, y que diga cuantas cifras tiene
+
+num = int(input("Introduce un número entero entre 0 y 99999: "))
+cifras = 0
+if 0 <= num <= 99999:
+    while num != 0:
+        num = num // 10
+        cifras = cifras + 1
