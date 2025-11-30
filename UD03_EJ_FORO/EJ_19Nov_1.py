c909f00fffe0d6d0cf1c4a87e97a3dd769244137
“Mar8787” <mss7@outlook.com>
2025-11-30 15:15:16 +0100

    Arreglado rombo hueco

--- UD03_EJ_FORO/EJ_19Nov_1.py
+++ UD03_EJ_FORO/EJ_19Nov_1.py
@@ -1,10 +1,10 @@
 #Imprime un diamante hueco de altura total 2n - 1, centrado con asteriscos, donde solo se imprimen los bordes y el centro.
-
-altura = int(input("Introduce el valor de la altura de la figura: "))
+altura = 5
+# altura = int(input("Introduce el valor de la altura de la figura: "))
 
 for i in range(0, (altura * 2) - 1):
     for j in range(0, (altura * 2) - 1):
-        if i == ((altura * 2) - 1)-1 or j == ((altura * 2) - 1)-1 or i == 0 or j == 0 or i == j // 2 - 1 :
+        if i + j == altura - 1 or j - i == altura - 1 or i - j == altura -1 or i + j == altura * 2 + 2:
             print("*", end=" ")
 
         else:
