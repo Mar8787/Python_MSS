e93c2cd07bd8d3127b8980c731bc460fd545285a
“Mar8787” <mss7@outlook.com>
2025-11-30 14:43:06 +0100

    Terminado bloque 2 los no repetidos

--- UD03_EJ_UD03_BL2/EJ_18.py
+++ UD03_EJ_UD03_BL2/EJ_18.py
@@ -1,2 +1,11 @@
-# 18. Crea una aplicación que dibuje una escalera de asteriscos. Nosotros le pasamos la altura de la escalera por teclado.
+# 18. Programa que calcule el valor A elevado a B (A^B) sin hacer uso del operador de potencia (^), siendo A y B valores introducidos por teclado,
+# y luego muestre el resultado por pantalla.
 
+a = int(input("Introduce un número entero: "))
+b = int(input("Introduce su potencia: "))
+
+resultado = 1
+
+for i in range(b):
+    resultado = resultado * a
+print(resultado)
